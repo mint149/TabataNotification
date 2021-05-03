9ae45884c1d07bfdc895f9d13544778c43b2680f
Mint <skylove.exe@gmail.com>
2021-05-03 14:18:11 +0900

    Initial Commit

---
+++ TabataNotification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabataNotification
+//
+//  Created by hato on 2021/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabataNotification/TabataNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabataNotificationApp.swift
+//  TabataNotification
+//
+//  Created by hato on 2021/05/03.
+//
+
+import SwiftUI
+
+@main
+struct TabataNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
