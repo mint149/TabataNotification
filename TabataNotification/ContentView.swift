f9ac15df3d6b0e659e5eda09f3e2dc6ee8932cde
Mint <skylove.exe@gmail.com>
2021-05-04 10:28:01 +0900

    タバタトレーニングのインターバル設定を実装

--- TabataNotification/ContentView.swift
+++ TabataNotification/ContentView.swift
@@ -8,25 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum StatusCode {
+        case Ready
+        case Workout
+        case Interval
+    }
+
     @State var timer : Timer!
     @State var time = 5
+    @State var setCount = 0
+    @State var statusText = "Ready..."
+    @State var statusCode = StatusCode.Ready
     
     var body: some View {
         VStack{
-            Text("\(time)")
+            Text("Tabata Timer")
+                .font(.largeTitle)
+            Spacer()
+            Text(statusText)
+                .font(.largeTitle)
+            Text(String(time))
+                .font(.largeTitle)
+            Spacer()
             Button(action: {
+                // 変数初期化
                 time = 5
+                setCount = 0
+                statusText = "Ready..."
+                statusCode = StatusCode.Ready
                 timer?.invalidate()
+                
+                // タイマー開始
                 timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ (tim) in
                     time -= 1
                     if time == 0 {
-                        timer?.invalidate()
+                        switch statusCode {
+                        case .Ready:
+                            statusCode = .Workout
+                            statusText = "Workout"
+                            time = 20
+                        case .Workout:
+                            setCount += 1
+                            statusCode = .Interval
+                            statusText = "Interval"
+                            time = 10
+                        case .Interval:
+                            if setCount == 8 {
+                                statusText = "Finish!"
+                                timer?.invalidate()
+                            } else {
+                                statusCode = .Workout
+                                statusText = "Workout"
+                                time = 20
+                            }
+                        }
                     }
                 }
                 
             }) {
                 Text("Start")
+                    .fontWeight(.bold)
             }
+            Spacer()
         }
     }
 }
