a4d0e9859969a7c08caca70f56cedad09ff12aac
Mint <skylove.exe@gmail.com>
2021-05-04 09:54:45 +0900

    5秒カウントダウン処理を実装

--- TabataNotification/ContentView.swift
+++ TabataNotification/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var timer : Timer!
+    @State var time = 5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("\(time)")
+            Button(action: {
+                time = 5
+                timer?.invalidate()
+                timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ (tim) in
+                    time -= 1
+                    if time == 0 {
+                        timer?.invalidate()
+                    }
+                }
+                
+            }) {
+                Text("Start")
+            }
+        }
     }
 }
 
